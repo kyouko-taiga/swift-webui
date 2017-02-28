f1066ba580c42ec403ef17d205ce94378421dbac
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-02-28 13:41:08 +0100

    Implements the file tree UI.

--- src/components/editor/FileTree.js
+++ src/components/editor/FileTree.js
@@ -4,80 +4,83 @@ import { connect } from 'react-redux'
 
 
 class FileTree extends React.Component {
+
     render() {
-        let subdirectories = this.props.subdirectories.map((name) =>
-            <button key={name} className="list-group-item">
-                <i className="fa fa-fw fa-folder-o" /> { name }
-            </button>
-        )
-
-        let files = this.props.files.map((file) => {
-            const classname = classNames({
-                'list-group-item': true,
-                'list-group-item-warning':
-                    this.props.currentFile && (file.sha == this.props.currentFile.sha)
-            })
-
-            return (
-                <button key={file.sha} className={classname}>
-                    <i className="fa fa-fw fa-file-text-o" /> { file.name }
-                </button>
-            )
-        })
+        const nodes = makeNodes(createArborescence(this.props.files))
 
         return (
             <div>
                 <div className="panel panel-info">
                     <div className="panel-heading">
                         <i className="fa fa-fw fa-code-fork" />
-                        <span className="text-muted">Branch: </span>
-                        <b>{ this.props.currentBranch.name }</b>
-                        <div className="pull-right">
-                            <i className="fa fa-fw fa-chevron-down" />
-                        </div>
+                        { this.props.repository.name }/{ this.props.repository.currentBranch }
                     </div>
-                    <div className="list-group">
-                        { subdirectories }{ files }
-                    </div>
+                    <ul className="sw-filetree">
+                        { nodes }
+                    </ul>
                 </div>
             </div>
         )
     }
+
+}
+
+
+function createArborescence(files) {
+    let arborescence = {}
+
+    for (let path in files) {
+        // Walk the arborescence and create any missing subdirectories.
+        let directory = arborescence
+        for (let subdirectory of path.split('/').slice(0, -1)) {
+            if (!(subdirectory in directory)) {
+                directory[subdirectory] = {}
+            }
+            directory = directory[subdirectory]
+        }
+
+        // Place the file in the arborescence.
+        directory[path] = files[path]
+    }
+
+    return arborescence
+}
+
+
+function makeNodes(arborescence, depth=0) {
+    let nodes = []
+    const indentation = Array(depth).fill().map(
+        (_, i) => <span key={i} className="sw-filetree-indent"></span>)
+
+    for (let node of Object.keys(arborescence).sort()) {
+        if ('mimetype' in arborescence[node]) {
+            nodes.push(
+                <li key={`${node}${depth}`}>
+                    { indentation }
+                    <i className="fa fa-fw fa-file-text-o" /> { arborescence[node].name }
+                </li>
+            )
+        } else {
+            nodes.push(
+                <li key={`${node}${depth}`}>
+                    { indentation }
+                    <i className="fa fa-fw fa-folder-o" /> { node }
+                </li>
+            )
+
+            nodes = nodes.concat(makeNodes(arborescence[node], depth + 1))
+        }
+    }
+
+    return nodes
 }
 
 
 function stateToProps(state) {
-    // Get the current branch.
-    const currentBranch = state.branches[state.fileTree.branch]
-
-    // Filter the branch files so we keep only that of the current directory.
-    const directory = state.fileTree.directory
-    let re = RegExp('^' + directory.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'))
-    re.compile()
-
-    let files = []
-    let subdirectories = []
-    for (let sha of currentBranch.files) {
-        // Remove the prefix of the current directory from the filepath. Then,
-        // if there's still a '/' in the remainder, we can assume the file is
-        // nested in some subdirectory.
-        const file = state.files[sha]
-        const stem = file.path.replace(re, '')
-        const slashIndex = stem.indexOf('/')
-        if (slashIndex == -1) {
-            files.push(file)
-        } else {
-            // If the file is nested, we keep a reference to its closest
-            // ancestor, w.r.t. the current directory.
-            subdirectories.push(stem.substring(0, slashIndex))
-        }
-    }
-
     return {
-        currentBranch: currentBranch,
-        currentFile: state.files[state.fileTree.file],
-        files: files,
-        subdirectories: subdirectories
+        repository: state.repository,
+        files: state.files,
+        editor: state.editor
     }
 }
 
--- src/reducers.js
+++ src/reducers.js
@@ -1,54 +1,34 @@
+import { combineReducers } from 'redux'
+
 
 const initialState = {
-    repositories: {
-        '12345':  {
-            id: '12345',
-            name: 'swift-webui',
-            branches: ['kyouko-taiga/foo/master', 'kyouko-taiga/foo/dev']
-        }
-    },
-    branches: {
-        'kyouko-taiga/foo/master': {
-            id: 'kyouko-taiga/foo/master',
-            repository: '12345',
-            name: 'master',
-            files: ['012c1', 'ebb48', 'a672d']
-        },
-        'kyouko-taiga/foo/dev': {
-            id: 'kyouko-taiga/foo/dev',
-            repository: '12345',
-            name: 'dev',
-            files: ['012c1', 'ebb48']
-        }
+    repository: {
+        id: '12345',
+        name: 'swift-webui',
+        currentBranch: 'master'
     },
     files: {
-        '012c1': {
-            sha: '012c1',
-            type: 'file',
-            name: 'README.md',
+        'README.md': {
+            mimetype: 'text/markdown',
             path: 'README.md',
+            name: 'README.md',
             content: 'This is a README file.'
         },
-        'ebb48': {
-            sha: 'ebb48',
-            type: 'file',
+        'LICENSE': {
+            mimetype: 'text/plain',
+            name: 'LICENSE',
+            path: 'LICENSE',
+            content: 'MIT LICENSE'
+        },
+        'Sources/main.swift': {
+            mimetype: 'text/x-swift',
             name: 'main.swift',
             path: 'Sources/main.swift',
             content: 'print("So swifty!")'
-        },
-        'a672d': {
-            sha: 'a672d',
-            type: 'file',
-            name: 'LICENSE',
-            path: 'LICENSE',
-            content: 'MIT'
         }
     },
-    fileTree: {
-        repository: '12345',
-        branch: 'kyouko-taiga/foo/master',
-        file: '012c1',
-        directory: '/'
+    editor: {
+        currentFile: 'Sources/main.swift'
     }
 }
 
