ca0048a2d601891020ac1c4a64bf25528d92eec7
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-03-01 01:59:26 +0100

    Implements file saving for the editor.

--- server-mock.py
+++ server-mock.py
@@ -1,19 +1,6 @@
 import json
 
-from flask import Flask, current_app, request
-
-
-def jsonify_list(data):
-    indent = None
-    separators = (',', ':')
-
-    if current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] \
-       and not request.is_xhr:
-        indent = 2
-        separators = (', ', ': ')
-
-    data = json.dumps(data, indent=indent, separators=separators, cls=current_app.json_encoder)
-    return current_app.response_class((data, '\n'), mimetype='application/json')
+from flask import Flask, current_app, jsonify, request
 
 
 database = {
@@ -41,6 +28,26 @@ database = {
 }
 
 
+class ApiError(Exception):
+
+    def __init__(self, message, http_status=400):
+        super(ApiError, self).__init__(message)
+        self.http_status = http_status
+
+
+def jsonify_list(data):
+    indent = None
+    separators = (',', ':')
+
+    if current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] \
+       and not request.is_xhr:
+        indent = 2
+        separators = (', ', ': ')
+
+    data = json.dumps(data, indent=indent, separators=separators, cls=current_app.json_encoder)
+    return current_app.response_class((data, '\n'), mimetype='application/json')
+
+
 app = Flask(__name__)
 
 
@@ -53,6 +60,18 @@ def index():
 @app.route('/api/repositories/<repository_id>/files/')
 def list_files(repository_id):
     return jsonify_list(database['files'])
+
+
+@app.route('/api/repositories/<repository_id>/files/<path:file_path>', methods=['PATCH'])
+def patch_files(repository_id, file_path):
+    post_data = request.get_json(force=True)
+
+    for i in range(len(database['files'])):
+        if database['files'][i]['path'] == file_path:
+            database['files'][i] = post_data
+            return jsonify(database['files'][i])
+
+    raise ApiError('file not found', 404)
 
 
 if __name__ == '__main__':
--- src/reducers/files.js
+++ src/reducers/files.js
@@ -1,5 +1,6 @@
 import {
     LIST_FILES,
+    GET_FILE,
     PATCH_FILE,
     EDITOR_UPDATE_FILE_CONTENT
 } from '../actions/types'
@@ -8,13 +9,17 @@ import {
 const files = (state = {}, action) => {
     switch (action.type) {
     case PATCH_FILE: // TODO: Handle optimistic update.
+    case GET_FILE:
     case LIST_FILES:
         if ((action.meta.status == 'pending') || !action.payload.entities) {
             return state
         }
 
-        return Object.map(action.payload.entities['files'], (key, value) =>
+        return {
+            ...state,
+            ...Object.map(action.payload.entities['files'], (key, value) =>
             [key, {...value, __modified__: false}])
+        }
 
     case EDITOR_UPDATE_FILE_CONTENT:
         const file = state[action.payload.filePath]
