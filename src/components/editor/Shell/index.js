148bd7f6302536ca97da4c36cccecce55a507dfd
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-03-03 02:02:04 +0100

    Implements a shell function to open files in the editor.

--- src/components/editor/Shell/index.js
+++ src/components/editor/Shell/index.js
@@ -1,14 +1,53 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
+import { changeActiveFile } from '../../../actions/editor'
 import Terminal from './Terminal'
+import { Errors } from './Terminal/const'
+import * as Util from './Terminal/util'
 
 
 class Shell extends Terminal {
 
+    constructor(props) {
+        super(props)
+
+        const open = {
+            exec: (state, { args }) => {
+                const relativePath = args[0] || ''
+                const fullPath = Util.extractPath(relativePath, state.cwd)
+                const components = fullPath.split('/')
+                const dirname = components.slice(0, -1).join('/')
+                const filename = components.slice(-1)
+                const { err, dir } = Util.getDirectoryByPath(state.structure, dirname)
+
+                if (err) {
+                    return Util.appendError(state, err, path)
+                } else if (!dir[filename]) {
+                    return Util.appendError(state, Errors.NO_SUCH_FILE, path)
+                } else if (!dir[filename].hasOwnProperty('content')) {
+                    return Util.appendError(state, Errors.IS_A_DIRECTORY, path)
+                } else {
+                    props.dispatch(changeActiveFile(fullPath))
+                }
+
+                return state
+            }
+        }
+
+        this.extensions = { open }
+    }
+
     render() {
         // console.log(this.props.structure)
-        return <Terminal prefix="" structure={this.props.structure} theme={'dark'} />
+        return (
+            <Terminal
+              prefix=""
+              structure={this.props.structure}
+              extensions={this.extensions}
+              theme={'dark'}
+            />
+        )
     }
 
 }
@@ -24,7 +63,7 @@ function arborescenceToStructure(arborescence, files) {
     let result = {}
     for (const key in arborescence) {
         if (typeof arborescence[key] === 'string') {
-            result[key] = files[arborescence[key]]
+            result[key.split('/').slice(-1)] = files[arborescence[key]]
         } else {
             result[key] = arborescenceToStructure(arborescence[key].files, files)
         }
