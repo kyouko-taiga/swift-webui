7e0825f2ee453361965fa092cbc1dcc66ee54cb4
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-03-03 01:31:26 +0100

    Maps the file architecture to the shell's filesystem.

--- src/components/editor/Shell/index.js
+++ src/components/editor/Shell/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+
 import Terminal from './Terminal'
 
 
-export default class Shell extends Terminal {
+class Shell extends Terminal {
 
     render() {
-        return <Terminal prefix="" theme={'dark'} />
+        // console.log(this.props.structure)
+        return <Terminal prefix="" structure={this.props.structure} theme={'dark'} />
     }
 
 }
+
+Shell.propTypes = {
+    structure: PropTypes.object.isRequired,
+}
+
+
+// Transform the file arborescence so that it is compatible with the shell's
+// filesystem representation.
+function arborescenceToStructure(arborescence, files) {
+    let result = {}
+    for (const key in arborescence) {
+        if (typeof arborescence[key] === 'string') {
+            result[key] = files[arborescence[key]]
+        } else {
+            result[key] = arborescenceToStructure(arborescence[key].files, files)
+        }
+    }
+    return result
+}
+
+
+function stateToProps(state) {
+    return {
+        structure: arborescenceToStructure(state.arborescence, state.files),
+    }
+}
+
+export default connect(stateToProps)(Shell)
